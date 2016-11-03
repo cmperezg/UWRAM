8ab8e5e92b2ebc2e4ff813eb20f46f5ce4c2424e
cmperezg <cmpgt@outlook.es>
2016-11-03 18:22:56 -0400

    functional wbmh

--- wordbmh.cpp
+++ wordbmh.cpp
@@ -9,6 +9,17 @@
 #include <nmmintrin.h>
 
 //compile: g++ -g wordbmh.cpp -o wordbmh -std=c++11 -msse4.2
+const int mode = _SIDD_UBYTE_OPS|_SIDD_CMP_EQUAL_EACH|_SIDD_MOST_SIGNIFICANT|_SIDD_NEGATIVE_POLARITY;
+
+void print128_ch(__m128i var){
+
+    uint8_t *val = (uint8_t*) &var;
+    printf("Text: %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c \n",
+           val[0], val[1], val[2], val[3], val[4], val[5],
+           val[6], val[7], val[8], val[9], val[10], val[11],
+           val[12], val[13], val[14], val[15]);
+}
+
 
 std::list<int> wbmh(std::string text, std::string pat){
 	//store starting index of occurrences here
@@ -27,18 +38,25 @@ std::list<int> wbmh(std::string text, std::string pat){
 		delta12[pat.at(j)] = patlen-j-1;
 	}
 	char lastch = pat.at(patlen-1);
+	//std::cout<< delta12[lastch] << lastch <<"\n";
+	//if(delta12[lastch]==0) delta12[lastch]=patlen;
 	__m128i packedpat = _mm_loadu_si128 ((__m128i *) pat.c_str());
 	//const char *s = astring.substr(0,16).c_str();
 	unsigned int patleni = patlen-1;
 	unsigned int i = patleni;
+	int ind;
 	
 	while(i<strlen){
-		__m128i window = text.substr(i-patleni,i).c_str();
-		int ind = _mm_cmpistri(pat,window,mode);
+		__m128i window = _mm_loadu_si128 ((__m128i *)(text.substr(i-patleni,patlen).c_str()));
+		ind = _mm_cmpestri(packedpat,patlen,window,patlen,mode);
+		//std::cout << "substring: " << text.substr(i-patleni,patlen) << "\n";
+		//std::cout<<"cmpistri: " << ind << "\n\n";
 		if(ind>patleni){
 			//pattern found
 			res.push_back(i-patleni);
-			printf("found at: %d\n",i-patleni);
+			//printf("found at: %d\n",i-patleni);
+			//print128_ch(window);
+			
 			i = i+delta12[lastch];
 		}else{
 			i = i+delta12[text.at(i-(patlen-ind)+1)];
@@ -47,6 +65,15 @@ std::list<int> wbmh(std::string text, std::string pat){
 	return res;
 }
 
+void printResults(const std::list<int>& s){
+	std::cout<< "found pattern at: ";
+	std::list<int>::const_iterator i;
+	for(i=s.begin();i!=s.end();++i){
+		std::cout<<*i << " ";
+	}
+	std::cout<< "\n";
+}
+
 int main(){
-	wbmh("banana","nana");
+	printResults(wbmh("he helped her","he"));
 }
