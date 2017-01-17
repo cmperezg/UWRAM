2f7bc4c1e34569e6c64dbe3ab53d4b30c4cd078e
cmperezg <cmpgt@outlook.es>
2017-01-17 17:16:04 -0500

    progress on wlcs

--- LCS/wlcs.cpp
+++ LCS/wlcs.cpp
@@ -50,12 +50,15 @@ int lcslength(std::string X, std::string Y){
 	
 	H[0][1] = 0;
 	V[1][0] = 0;
-	
+	int cpw = w/f;
+	std::cout<<"cpw: "<<cpw<<"\n";
 	int length = 0;
 	int k; //diagonal counter
 	for(k=2;k<=(m+n);k++){
+		std::cout<<"k: "<<k<<"\n";
 		//length of the diagonal
 		int l = min(n,k-1) + min(m,k-1) - k + 1;
+		std::cout<<"l: "<<l<<"\n";
 		//relevant substring index for Y
 		int j1 = k - min(m,k-1);
 		int j2 = min(n,k);
@@ -66,24 +69,45 @@ int lcslength(std::string X, std::string Y){
 		int j = j1;
 		int i = i2;
 		int s = ceil(double(l*f)/double(w)); //words needed to load diagonal
+		
 		int t;
+		std::cout<<"s: "<<s<<"\n";
 		for(t=1;t<=s;t++){
-			int jp = min(j+s-1,j2);
-			int ip = max(i+s-1,i1);
-			  
-			long long int wy;
-			long long int wx;
+			//int jp = min(j+s-1,j2);
+			int jp = min(j2,t*cpw);
+			//int ip = max(i+s-1,i1);
+			int ip = max(i1,i2-(t*cpw));
+			 std::cout<<"i: "<<i<<"\n";
+			std::cout<<"i+s-1: "<<i+s-1<<"\n";
+			std::cout<<"i2: "<<i2<<"\n";
+			  std::cout<<"t: "<<t<<"\n";
+			 
+			  std::cout<<"ip: "<<ip<<"\n";
+			long long int wy=0;
+			long long int wx=0;
 			//pack wy 
-			int i =1;
-			for(c =j; c<=jp; c++){
-				int sh = w-i*f;
-				wy = wy | Y.at(c-1)<<sh;
-				std::cout<<Y.at(c-1);
-				i++;
-			}
+			//int i =1;
 			std::cout<<"\n wy: ";
-			printlongbits(wy);                                                    
-			
+			//std::cout<<Y.at(c-1)<<"\n";
+			for(c =j; c<=jp; c++){
+				wy = wy<<f | Y.at(c-1);
+				std::cout<<Y.at(c-1);
+				//i++;
+			}
+			std::cout<<"\n";
+			printlongbits(wy); 
+			//pack wx (reverse order)
+			std::cout<<"\n wx: ";
+			//std::cout<<X.at(c-1)<<"\n";
+			for(c = i; c>=ip; c--){
+				wx = wx<<f | X.at(c-1);
+				std::cout<<X.at(c-1);
+			}
+			   std::cout<<"\n";
+			//std::cout<<"\n wx: ";
+			printlongbits(wx);                                                 
+			i = ip+1;
+			j = jp+1;
 		}
 	}
 	
