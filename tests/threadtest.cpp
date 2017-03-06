3dc7ba8d78fc17fa6a4d63c551aba7e9db7cacfc
Camila Perez-Gavilan <cmpgt@outlook.es>
2017-03-06 14:53:02 -0500

    added thread test

---
+++ tests/threadtest.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <thread>
+
+//compile: g++ -g threadtest.cpp -o threadtest -pthread -std=c++11
+
+
+  
+static const int num_threads = 4;
+  
+//This function will be called from a thread
+  
+void call_from_thread(int tid) {
+	std::cout << "Launched by thread " << tid << std::endl;
+}
+ 
+int main() {
+    std::thread t[num_threads]; 
+	//Launch a group of threads
+	for (int i = 0; i < num_threads; ++i) {
+		t[i] = std::thread(call_from_thread, i);
+	}
+	
+	std::cout << "Launched from the main\n";
+ 
+	//Join the threads with the main thread
+	for (int i = 0; i < num_threads; ++i) {
+		t[i].join();
+	}
+	
+	return 0;
+}
